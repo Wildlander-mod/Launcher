8cf00fae1ab48e1e2eb5324d7ddf5906f2b92729
Thomas Blasquez <31079038+RingComics@users.noreply.github.com>
2020-09-23 12:36:36 -0600

    ENB and Launcher settings complete
    All ENB Manager functions are done, as well as the launcher options menu

--- app/js/reqs.js
+++ app/js/reqs.js
@@ -1,5 +1,5 @@
 const { shell,remote } = require('electron');
-const fs = require('fs');
+const fs = require('fs-extra');
 const Dialogs = require('dialogs')
 const dialogs = Dialogs()
 let currentConfig = require(__dirname + '\\config\\config.json')
