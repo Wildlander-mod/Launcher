8257cb6ce6643cea8087bfb6e998b86804fe02b7
James Williams <jowilliams12000@gmail.com>
2020-09-23 21:57:05 -0400

    vue-integration: Progress commit.

---
+++ src/components/LeftPanel.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>UltSky</h1>
+</template>
+
+<script>
+export default {
+  name: 'LeftPanel'
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container fluid>
+    <b-row no-gutters>
+      <b-col cols="3">
+        <LeftPanel/>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import LeftPanel from '../components/LeftPanel.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    LeftPanel
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
