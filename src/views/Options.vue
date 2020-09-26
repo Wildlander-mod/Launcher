1658ff11cb01ab59db1ac22f64e51fa4301edbda
Thomas Blasquez <31079038+RingComics@users.noreply.github.com>
2020-09-26 01:13:52 -0600

    Game launch capability
    Still needs to be built and tested in a UltSky install, but the Launch Game and Open MO2 buttons should work now if the launcher is in it's own folder in the UltSky/content directory

--- src/views/Options.vue
+++ src/views/Options.vue
@@ -2,24 +2,27 @@
   <div>
     <h2>Launcher Settings</h2>
     <b-form @submit="saveConfig">
-      <span>Skyrim directory: {{ this.GameDirectory }}</span>
+      <span>Skyrim directory: <p>{{ this.GameDirectory }}</p></span>
       <b-button @click="showModal('open-folder-dialog')">Browse</b-button><br/><br/>
+
       <span>Default Preset</span>
       <b-select v-model="DefaultProfile" label="Performance Profile">
         <b-select-option value="Low" class="ml-2">Low</b-select-option>
         <b-select-option value="Medium" class="ml-2">Medium</b-select-option>
         <b-select-option value="High" class="ml-2">High</b-select-option>
       </b-select>
+
       <b-button type="submit" variant="primary">Apply</b-button>
-      <b-modal ref="open-folder-dialog" title="Delete Your Profile" hide-footer>
-        <b-form @submit="getDirectory">
-          <p class="text-center">
-            Please select your Skyrim installation folder
-          </p>
-          <b-button type="submit" variant="outline-primary">OK</b-button>
-        </b-form>
-      </b-modal>
     </b-form>
+
+    <b-modal ref="open-folder-dialog" title="Get Skyrim Directory" hide-footer>
+      <b-form @submit="getDirectory">
+        <p class="text-center">
+          Please select your Skyrim installation folder
+        </p>
+        <b-button type="submit" variant="outline-primary">OK</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
