00264600914d62ca44ef2cd5996b1e48b5e54a84
Thomas Blasquez <31079038+RingComics@users.noreply.github.com>
2021-01-26 21:20:12 -0800

    Implemented IPC Handling
    * IPC calls can now be made on the front end. IPC handlers are in ipcHandlers.js
    * Configuration file initialization was implemented, also in the ipcHandlers.js temporarily
    * Minimize and Close buttons now work properly
    * Added a cheeky link to Azura's Star (added mostly for testing, can remove if needed.

--- ultskylauncher/src/components/TitleBar.vue
+++ ultskylauncher/src/components/TitleBar.vue
@@ -35,6 +35,14 @@ export default {
   name: 'TitleBar',
   components: {
     Logo
+  },
+  methods: {
+    close () {
+      window.ipcRenderer.send('close')
+    },
+    minimize () {
+      window.ipcRenderer.send('minimize')
+    }
   }
 }
 </script>
