38588af8f8a06ed7659a788b72a4b06e7c249ed5
Matt Lishman <matt.lishman@gmail.com>
2021-09-01 17:40:57 +0100

    feat: add game files tab

---
+++ src/components/GameFolderFiles.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="l-column c-game-folder-files">
+    <div class="l-column c-game-files">
+      <div class="l-row c-game-files__copy">
+        <BaseButton
+          class="c-game-folder-files__action"
+          size="large"
+          type="primary"
+        >
+          Copy game files
+        </BaseButton>
+        <div class="l-flex l-center">
+          <p>
+            Copy all Ultimate Skyrim game files (not enb files) to the Skyrim
+            directory
+          </p>
+        </div>
+      </div>
+      <div class="l-row">
+        <BaseButton
+          class="c-game-folder-files__action"
+          size="large"
+          type="warning"
+        >
+          Delete game files
+        </BaseButton>
+        <div class="l-flex l-center">
+          <p>
+            Delete all Ultimate Skyrim game files (not enb files) from the
+            Skyrim directory
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="l-column c-enb-files">
+      <div class="l-row c-enb-files-copy">
+        <BaseButton
+          class="c-game-folder-files__action"
+          size="large"
+          type="primary"
+        >
+          Copy enb files
+        </BaseButton>
+        <div class="l-flex l-center">
+          <p>Copy all Ultimate Skyrim enb files to the Skyrim directory</p>
+        </div>
+      </div>
+      <div class="l-row">
+        <BaseButton
+          class="c-game-folder-files__action"
+          size="large"
+          type="warning"
+        >
+          Delete enb files
+        </BaseButton>
+        <div class="l-flex l-center">
+          <p>Delete all Ultimate Skyrim enb files from the Skyrim directory</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import BaseButton from "@/components/BaseButton.vue";
+
+@Options({
+  components: { BaseButton },
+})
+export default class GameFolderFiles extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss";
+
+.c-game-folder-files {
+  width: 100%;
+}
+
+.c-game-files {
+  border-bottom: 1px solid $colour-background--light;
+
+  padding-bottom: $size-spacing--large;
+}
+
+.c-game-folder-files__action {
+  margin-right: $size-spacing;
+}
+
+.c-game-files__copy,
+.c-enb-files-copy {
+  margin-bottom: $size-spacing;
+}
+
+.c-enb-files {
+  margin-top: $size-spacing--large;
+}
+</style>
---
+++ src/views/ViewGameFiles.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppPage>
+    <AppPageContent title="Game files">
+      <GameFiles />
+    </AppPageContent>
+  </AppPage>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import AppPage from "@/components/AppPage.vue";
+import AppPageContent from "@/components/AppPageContent.vue";
+import GameFolderFiles from "@/components/GameFolderFiles.vue";
+
+@Options({
+  components: { GameFiles: GameFolderFiles, AppPageContent, AppPage },
+})
+export default class ViewGameFiles extends Vue {}
+</script>
