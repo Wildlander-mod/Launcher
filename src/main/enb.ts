2f61726fc9c558081a6f6583ef6bd270e6941771
TaranRoth <taranr5080@gmail.com>
2021-09-02 12:06:20 -0400

    feat: add action to delete enb files closes #181

--- src/main/enb.ts
+++ src/main/enb.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import { logger } from "@/main/logger";
+
 export const enbFiles = [
   "enbseries.ini",
   "enblocal.ini",
@@ -6,3 +9,18 @@ export const enbFiles = [
   "d3dx9_42.dll",
   "enbhost.exe",
 ];
+
+export async function deleteEnbFiles(skyrimDirectory: string) {
+  logger.info("Deleting ENB Files");
+
+  const existingEnbFiles = enbFiles.filter((file) =>
+    fs.existsSync(`${skyrimDirectory}/${file}`)
+  );
+  const enbFilesWithoutEnbseries = existingEnbFiles.filter(
+    (file) => file !== "enbseries"
+  );
+  enbFilesWithoutEnbseries.forEach((file) =>
+    fs.promises.unlink(`${skyrimDirectory}/${file}`)
+  );
+  fs.rmdirSync(`${skyrimDirectory}/enbseries`, { recursive: true });
+}
