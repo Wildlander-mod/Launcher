0a568319934a689689549df4e2d9f87a7c1d9e4c
Phinocio <github@phinocio.com>
2021-09-02 10:44:38 -0600

    chore: fix line endings error (#196)
    * Setting endOfLine to auto prevents constant line ending errors on Windows
    * Change " to ' for vue/prettier to be consistent across the file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,9 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    "@vue/typescript/recommended",
+    '@vue/typescript/recommended',
     '@vue/prettier',
-    "@vue/prettier/@typescript-eslint"
+    '@vue/prettier/@typescript-eslint'
   ],
 
   parserOptions: {
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': ['error', { 'endOfLine': 'auto' }]  
   }
 }
