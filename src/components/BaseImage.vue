dbc3e39a335bf3593229fe8d207ab0fabb206027
Matt Lishman <matt.lishman@gmail.com>
2021-11-30 17:07:49 +0000

    chore: move all svgs to public folder instead of components

---
+++ src/components/BaseImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="imageSource" :alt="alt" height="50" width="50" />
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+export default class BaseImage extends Vue {
+  @Prop({ require: true }) imageSource!: string;
+  @Prop({ required: true }) alt!: string;
+}
+</script>
---
+++ src/components/ImageWithText.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="l-column l-center">
+    <BaseImage :image-source="imageSource" :alt="alt" />
+    <div class="c-svg-with-text__text">{{ text }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import BaseImage from "@/components/BaseImage.vue";
+
+@Options({
+  components: { BaseImage },
+})
+export default class ImageWithText extends Vue {
+  @Prop({ require: true }) imageSource!: string;
+  @Prop({ required: true }) alt!: string;
+  @Prop({ required: true }) text!: string;
+}
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss";
+
+.c-svg-with-text__text {
+  margin-top: $size-spacing;
+}
+</style>
