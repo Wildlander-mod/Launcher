4a0c6962cf16812277eae423be22fbda29321375
Matt Lishman <matt.lishman@gmail.com>
2021-12-15 18:13:50 +0000

    style: remove excess space at the top of the logo

--- src/components/TheTitleBar.vue
+++ src/components/TheTitleBar.vue
@@ -2,10 +2,10 @@
   <div class="c-title-bar l-row l-end" :class="{ 'l-no-flex-grow': !grow }">
     <div class="c-title-bar__navigation-bar"></div>
     <div class="c-title-bar__control" @click="minimize">
-      <span class="material-icons"> remove </span>
+      <span class="material-icons c-title-bar__control-icon"> remove </span>
     </div>
     <div class="c-title-bar__control" @click="close">
-      <span class="material-icons"> close </span>
+      <span class="material-icons c-title-bar__control-icon"> close </span>
     </div>
   </div>
 </template>
@@ -33,10 +33,15 @@ export default class TheTitleBar extends Vue {
 @import "~@/assets/scss";
 
 .c-title-bar {
-  height: 24px;
+  height: $size-spacing--titlebar;
 
   // The modal used sets its background-blur z-index to 1000. The title bar needs to be able to go over the top.
   z-index: 2000;
+}
+
+.c-title-bar__control-icon {
+  // This needs to match the size of the bar itself
+  font-size: $size-spacing--titlebar;
 }
 
 .c-title-bar__control {
