6257cb825dcc8be89602f1aef2c46ff0b6e4efa7
Phinocio <github@phinocio.com>
2022-01-15 10:39:10 -0700

    chore: remove close skyrim button
    It's been causing some issues, so for now, simply yeet the button for now

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -65,12 +65,9 @@
   <AppModal :show-modal="gameRunning" name="gameRunning">
     <div class="l-column l-center">
       <div class="u-spacing">
-        Skyrim is currently launching/running. To use the launcher, please close
-        Skyrim first.
+        Skyrim is currently launching/running, please wait. This is not an
+        error, and the launching process may take several minutes.
       </div>
-      <BaseButton type="primary" size="large" @click="closeGame"
-        >Close Skyrim
-      </BaseButton>
     </div>
   </AppModal>
 </template>
