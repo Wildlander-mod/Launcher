fb8ccd943e5a2ddbc74361580ae59b90e09515ac
TaranRoth <taranr5080@gmail.com>
2022-01-29 08:07:38 -0500

    chore: only auto update if device is connected

--- src/main/autoUpdate.ts
+++ src/main/autoUpdate.ts
@@ -7,6 +7,24 @@ import { isDevelopment } from "@/main/config";
 import fs from "fs";
 import { getWebContents } from "@/background";
 import { app } from "electron";
+import { networkInterfaces } from "os";
+
+async function isConnected(): Promise<boolean> {
+  const connections = networkInterfaces();
+  if (connections == undefined) return false;
+  let connected = false;
+  if (connections != undefined) {
+    Object.entries(connections).forEach((connectionGroup) => {
+      connectionGroup.forEach((subConnections) => {
+        if (typeof subConnections == "object")
+          subConnections.forEach((connection) => {
+            if (!connection["internal"]) connected = true;
+          });
+      });
+    });
+  }
+  return connected;
+}
 
 export async function autoUpdate() {
   autoUpdater.on(IPCEvents.UPDATE_AVAILABLE, () => {
@@ -34,9 +52,14 @@ export async function autoUpdate() {
     autoUpdater.quitAndInstall();
   });
 
-  // Only try to update if in production mode or there is a dev update file
+  // Only try to update if in production mode or there is a dev update file and the user is connected
   const devAppUpdatePath = path.join(__dirname, "../dev-app-update.yml");
-  if (isDevelopment && fs.existsSync(devAppUpdatePath)) {
+  if (!(await isConnected())) {
+    logger.debug(
+      "Skipping app update check because this device is not connected to the internet"
+    );
+    getWebContents().send(IPCEvents.UPDATE_NOT_AVAILABLE);
+  } else if (isDevelopment && fs.existsSync(devAppUpdatePath)) {
     logger.debug(`Setting auto update path to ${devAppUpdatePath}`);
     autoUpdater.updateConfigPath = devAppUpdatePath;
     await autoUpdater.checkForUpdates();
