174d4abe2e4e9fde7d717f85306643306ca0fd51
Matt Lishman <matt.lishman@gmail.com>
2022-03-16 15:07:43 +0000

    chore: add test modpack directory

--- src/main/modpack.ts
+++ src/main/modpack.ts
@@ -1,6 +1,8 @@
 import { backupOriginalENBs, copyENBFiles, getENBPresets } from "@/main/ENB";
 import { USER_PREFERENCE_KEYS, userPreferences } from "@/main/config";
-import { backupOriginalProfiles } from "@/main/modOrganizer";
+import { backupOriginalProfiles, MO2EXE } from "@/main/modOrganizer";
+import fs from "fs";
+import { logger } from "@/main/logger";
 
 export async function startupTasks() {
   await backupOriginalENBs();
@@ -11,4 +13,17 @@ export async function startupTasks() {
     (userPreferences.get(USER_PREFERENCE_KEYS.ENB_PROFILE) as string) ||
     (await getENBPresets())[0].real;
   await copyENBFiles(ENBFiles, false);
+}
+
+export async function checkModpackPathIsValid(modpackPath: string) {
+  return (
+    [MO2EXE, "profiles", "launcher"]
+      .filter((path) => !fs.existsSync(`${modpackPath}/${path}`))
+      .map((path) => {
+        logger.warn(
+          `Selected mod directory "${modpackPath}" doesn't contain a "${path}" directory/file`
+        );
+        return path;
+      }).length === 0
+  );
 }
