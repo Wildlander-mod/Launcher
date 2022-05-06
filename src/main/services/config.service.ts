73a47b7e46bd8b7ea2396d0670ca0bd622fe13cf
Matt Lishman <matt.lishman@gmail.com>
2022-05-06 22:19:18 +0100

    fix: prevent invalid enbs from being persisted on mod selection
    - improve startup logs by adding date
    
    closes #518

--- src/main/services/config.service.ts
+++ src/main/services/config.service.ts
@@ -3,6 +3,7 @@ import { USER_PREFERENCE_KEYS } from "@/shared/enums/userPreferenceKeys";
 import { Resolution } from "@/Resolution";
 import path from "path";
 import { BindingScope, injectable } from "@loopback/context";
+import { logger } from "@/main/logger";
 
 export const appRoot = path.resolve(`${__dirname}/../../`);
 export const isDevelopment = path.extname(appRoot) !== ".asar";
@@ -14,6 +15,13 @@ export interface UserPreferences {
   [USER_PREFERENCE_KEYS.PREVIOUS_ENB_PROFILE]: string;
   [USER_PREFERENCE_KEYS.RESOLUTION]: Resolution;
 }
+
+type PreferenceWithValidator = {
+  [key in keyof UserPreferences]?: {
+    value: UserPreferences[keyof UserPreferences];
+    validate?: (...args: unknown[]) => boolean | Promise<boolean>;
+  };
+};
 
 export const userPreferences = new Store<UserPreferences>({
   name: "userPreferences",
@@ -48,18 +56,30 @@ export class ConfigService {
   }
 
   setPreference(key: keyof UserPreferences | string, value: unknown) {
+    logger.debug(`Setting preference ${key} to ${value}`);
     return userPreferences.set(key, value);
   }
 
   /**
-   Only set the values if they don't already exist
+   Set the value specified if the key doesn't exist or the current value is invalid
    */
-  setDefaultPreferences(preferences: Partial<UserPreferences>) {
-    for (const [key, value] of Object.entries(preferences)) {
-      if (!userPreferences.has(key)) {
+  async setDefaultPreferences(preferences: PreferenceWithValidator) {
+    logger.debug("Setting default user preferences");
+    logger.debug(`Current preferences`);
+    logger.debug(this.getPreferences().store);
+    for (const [key, { value, validate }] of Object.entries(preferences)) {
+      const valid = validate ? await validate() : true;
+      if (!valid) {
+        logger.warn(
+          `Current ${key} preference is invalid. Setting to default: ${value}`
+        );
+      }
+      if ((!userPreferences.has(key) || !valid) && value) {
         this.setPreference(key, value);
       }
     }
+    logger.debug("New preferences");
+    logger.debug(this.getPreferences().store);
   }
 
   getPreferences() {
