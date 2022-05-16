762f0c57254a5ff76b2589addd5861909391e05a
Matt Lishman <matt.lishman@gmail.com>
2022-05-16 17:00:21 +0100

    feat: copy skyrim launch logs after game launch
    closes #511

--- src/main/services/game.service.ts
+++ src/main/services/game.service.ts
@@ -1,17 +1,24 @@
-import find from "find-process";
-import { logger } from "@/main/logger";
 import { BindingScope, injectable } from "@loopback/context";
+import { ConfigService } from "@/main/services/config.service";
+import { service } from "@loopback/core";
+import fs from "fs";
+import { logger } from "@/main/logger";
 
 @injectable({
   scope: BindingScope.SINGLETON,
 })
 export class GameService {
-  async closeGame() {
-    logger.info("Closing the game forcefully");
-    (await find("name", "SkyrimSE.exe")).forEach((skyrim) => {
-      logger.debug(`Found process to kill ${JSON.stringify(skyrim)}`);
-      process.kill(skyrim.pid);
-    });
-    logger.info("Killed all Skyrim processes");
+  constructor(@service(ConfigService) private configService: ConfigService) {}
+
+  async copySkyrimLaunchLogs() {
+    logger.info("Copying Skyrim launch logs");
+    const launchLogPath = `${this.configService.skyrimDirectory()}/d3dx9_42.log`;
+    const logPath = this.configService.getLogDirectory();
+    if (fs.existsSync(launchLogPath)) {
+      return fs.promises.copyFile(
+        launchLogPath,
+        `${logPath}/skyrim-launch-logs.log`
+      );
+    }
   }
 }
