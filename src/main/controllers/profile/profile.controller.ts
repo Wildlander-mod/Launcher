58d8a528ce0b6e0683edc61266cc63b72f989ab9
Matt Lishman <matt.lishman@gmail.com>
2022-06-16 18:47:17 +0100

    feat: refresh modpack when restoring profiles
    BREAKING CHANGE: When profiles are restored, the entire modpack will now
    be refreshed to keep everything in sync

--- src/main/controllers/profile/profile.controller.ts
+++ src/main/controllers/profile/profile.controller.ts
@@ -2,11 +2,13 @@ import { controller, handle } from "@/main/decorators/controller.decorator";
 import { ProfileService } from "@/main/services/profile.service";
 import { service } from "@loopback/core";
 import { PROFILE_EVENTS } from "@/main/controllers/profile/profile.events";
+import { LauncherService } from "@/main/services/launcher.service";
 
 @controller
 export class ProfileController {
   constructor(
-    @service(ProfileService) private profileService: ProfileService
+    @service(ProfileService) private profileService: ProfileService,
+    @service(LauncherService) private launcherService: LauncherService
   ) {}
 
   @handle(PROFILE_EVENTS.GET_PROFILES)
@@ -27,5 +29,6 @@ export class ProfileController {
   @handle(PROFILE_EVENTS.RESTORE_PROFILES)
   async restoreProfiles() {
     await this.profileService.restoreProfiles();
+    await this.launcherService.refreshModpack();
   }
 }
