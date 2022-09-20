974cfd75819ce561af4d1282a80ecab64fad78be
Matt Lishman <matt.lishman@gmail.com>
2022-09-20 19:20:06 +0100

    feat: support Wabbajack V3 modlist metadata files

--- src/main/services/wabbajack.service.ts
+++ src/main/services/wabbajack.service.ts
@@ -1,5 +1,9 @@
 import fs from "fs";
-import { WabbajackSettingsFile } from "@/wabbajack";
+import {
+  WabbajackInstallSettings,
+  WabbajackModpackMetadata,
+  WabbajackV2SettingsFile,
+} from "@/wabbajack";
 import { logger } from "@/main/logger";
 import { SystemService } from "@/main/services/system.service";
 import { service } from "@loopback/core";
@@ -11,26 +15,83 @@ import { ModpackService } from "@/main/services/modpack.service";
 })
 export class WabbajackService {
   private installedModpacksFilename = "installed_modlists.json";
-  private wabbajackInstalledModpacksPath = `${SystemService.getLocalAppData()}/Wabbajack/${
+  private wabbajackV2InstalledModpacksPath = `${SystemService.getLocalAppData()}/Wabbajack/${
     this.installedModpacksFilename
   }`;
+  private wabbajackV3InstalledModpacksPath = `${SystemService.getLocalAppData()}/Wabbajack/saved_settings`;
 
   constructor(
     @service(SystemService) private systemService: SystemService,
     @service(ModpackService) private modpackService: ModpackService
   ) {}
 
-  async getInstalledModpacks() {
-    if (fs.existsSync(this.wabbajackInstalledModpacksPath)) {
-      return JSON.parse(
-        await fs.promises.readFile(this.wabbajackInstalledModpacksPath, "utf-8")
-      ) as WabbajackSettingsFile;
+  async getInstalledModpacks(): Promise<WabbajackModpackMetadata | null> {
+    let v2Modpacks = {};
+    let v3Modpacks = {};
+    if (fs.existsSync(this.wabbajackV2InstalledModpacksPath)) {
+      v2Modpacks = await this.getInstalledModpacksFromWabbajackV2();
     } else {
-      logger.warn(
-        `${this.wabbajackInstalledModpacksPath} does not exist. No modpacks found.`
-      );
-      return null;
+      logger.warn(`${this.wabbajackV2InstalledModpacksPath} does not exist.`);
     }
+
+    if (fs.existsSync(this.wabbajackV3InstalledModpacksPath)) {
+      v3Modpacks = await this.getInstalledModpacksFromWabbajackV3();
+    } else {
+      logger.warn(`${this.wabbajackV3InstalledModpacksPath} does not exist.`);
+    }
+
+    const combined = {
+      ...v2Modpacks,
+      ...v3Modpacks,
+    };
+
+    return Object.keys(combined).length > 0 ? combined : null;
+  }
+
+  async getInstalledModpacksFromWabbajackV2(): Promise<WabbajackModpackMetadata> {
+    const modpackMeta = JSON.parse(
+      await fs.promises.readFile(this.wabbajackV2InstalledModpacksPath, "utf-8")
+    ) as WabbajackV2SettingsFile;
+    return Object.keys(modpackMeta)
+      .filter((key) => key !== "$type")
+      .reduce((accumulator, current) => {
+        return {
+          ...accumulator,
+          [modpackMeta[current].InstallationPath]: {
+            title: modpackMeta[current].ModList.Name,
+            installPath: modpackMeta[current].InstallationPath,
+            version: modpackMeta[current].ModList.Version,
+          },
+        } as WabbajackModpackMetadata;
+      }, {});
+  }
+
+  async getInstalledModpacksFromWabbajackV3(): Promise<WabbajackModpackMetadata> {
+    const files = (
+      await fs.promises.readdir(this.wabbajackV3InstalledModpacksPath)
+    ).filter((file) => file.startsWith("install-settings"));
+
+    const modlists = [];
+    for (const file of files) {
+      modlists.push(
+        JSON.parse(
+          await fs.promises.readFile(
+            `${this.wabbajackV3InstalledModpacksPath}/${file}`,
+            "utf-8"
+          )
+        ) as WabbajackInstallSettings
+      );
+    }
+    return modlists.reduce((accumulator, current) => {
+      return {
+        ...accumulator,
+        [current.InstallLocation]: {
+          title: current.Metadata?.title,
+          installPath: current.InstallLocation,
+          version: current.Metadata?.version,
+        },
+      };
+    }, {});
   }
 
   async getInstalledCurrentModpackPaths() {
@@ -40,8 +101,7 @@ export class WabbajackService {
       modpacks !== null
         ? Object.keys(modpacks).filter(
             (modpack) =>
-              modpack !== "$type" &&
-              modpacks[modpack].ModList.Name === modpackName
+              modpack !== "$type" && modpacks[modpack].title === modpackName
           )
         : [];
     logger.info(
@@ -61,8 +121,6 @@ export class WabbajackService {
   }
 
   async getModpackVersion() {
-    return (
-      (await this.getCurrentModpackMetadata())?.ModList.Version ?? "unknown"
-    );
+    return (await this.getCurrentModpackMetadata())?.version ?? "unknown";
   }
 }
