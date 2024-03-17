6e772b75eddbdeb7a482e3e52be90f6ea1b4e342
Matt Lishman <c060838@yara.com>
2024-03-17 21:29:22 +0000

    test: clean up dirent mock

--- src/__tests__/unit/helpers/mocks/dirent.mock.ts
+++ src/__tests__/unit/helpers/mocks/dirent.mock.ts
@@ -1,28 +1,12 @@
-/* istanbul ignore file */
-
 import type { Dirent } from "fs-extra";
+import sinon from "sinon";
 
-// TODO could all these methods be a sinon.stub instead and remove the ignore line?
 export const mockDirent: Omit<Dirent, "name"> = {
-  isFile() {
-    return true;
-  },
-  isBlockDevice() {
-    return false;
-  },
-  isCharacterDevice() {
-    return false;
-  },
-  isDirectory() {
-    return false;
-  },
-  isFIFO() {
-    return false;
-  },
-  isSocket() {
-    return false;
-  },
-  isSymbolicLink(): boolean {
-    return false;
-  },
+  isFile: sinon.stub().returns(true),
+  isBlockDevice: sinon.stub().returns(false),
+  isCharacterDevice: sinon.stub().returns(false),
+  isDirectory: sinon.stub().returns(false),
+  isFIFO: sinon.stub().returns(false),
+  isSocket: sinon.stub().returns(false),
+  isSymbolicLink: sinon.stub().returns(false),
 };
