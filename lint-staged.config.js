4faaefc7b80030f5035fc392515e4a191a8ea128
Matt Lishman <matt.lishman@gmail.com>
2025-05-14 12:27:59 +0100

    chore: remove e2e tests from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  "*.{ts,tsx,js,jsx,vue,json,scss,css}": ["npm run lint:fix", () => "npm run test"],
+  "*.{ts,tsx,js,jsx,vue,json,scss,css}": [
+    "npm run lint:fix",
+    () => "npm run test:unit",
+  ],
   "*.scss": ["npm run lint:styles"],
 };
