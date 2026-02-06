ca7d0429d2ea8d32caea95dba946737eb2881520
Matt Lishman <matt.lishman@gmail.com>
2026-02-06 22:09:53 +0000

    refactor: remove BaseInput component and use native input in Resolution

--- src/renderer/src/components/Resolution.vue
+++ src/renderer/src/components/Resolution.vue
@@ -36,14 +36,13 @@ import { Options, Vue } from "vue-class-component";
 import BaseDropdown, { SelectOption } from "./BaseDropdown.vue";
 import type { Resolution as ResolutionType } from "@/shared/types/Resolution";
 import BaseLink from "./BaseLink.vue";
-import BaseInput from "./BaseInput.vue";
 import logger from "electron-log/renderer";
 import { injectStrict, SERVICE_BINDINGS } from "../services/service-container";
 import { asyncFilter } from "@/shared/util/asyncFilter";
 import { RESOLUTION_EVENTS } from "@/main/controllers/resolution/resolution.events";
 
 @Options({
-  components: { BaseLink, BaseDropdown, BaseInput },
+  components: { BaseLink, BaseDropdown },
 })
 export default class Resolution extends Vue {
   selectedResolution: SelectOption | null = null;
